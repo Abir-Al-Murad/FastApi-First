0de5d517a738423f360b06fbc4b4fc18ae4dad01
AbirAlMurad <abiralmuradnub@gmail.com>
2026-02-08 10:23:36 +0600

    env file added and credential moves to it

--- app/database.py
+++ app/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine #database er sathe connection create korar jonno
 from sqlalchemy.ext.declarative import declarative_base #model class gulo base class create korbe
 from sqlalchemy.orm import sessionmaker
+from . config import settings
 
-SQLAlCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/sohojx'  #postgresql:username:password@servername/databasename
+SQLAlCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'  #postgresql:username:password@servername/databasename
 
 engine = create_engine(SQLAlCHEMY_DATABASE_URL)
 SesssionLocal = sessionmaker(autocommit =False,autoflush= False,bind=engine)
